b585c3b817f326d27340d6b8a9ff04e7d8fc48db
MegaBuilder2345 <elliotanselbaer@gmail.com>
2026-04-10 17:46:08 -0700

    tung sauhre

---
+++ lesson1/classwork/string_shortcuts.py
@@ -0,0 +1,18 @@
+first= "pinapple"
+last="Pizza"
+
+#string concatenation
+full = first + " " + last
+print(full)
+
+# repeat a string
+print("ha" *3)
+
+# 'in' checks if something appears inside a string
+sentence= "i like python"
+print("python" in sentence)
+print("java" in sentence)
+
+# Escape characters
+print("He said, \"hello\".")
+print("line 1\n line 2")
