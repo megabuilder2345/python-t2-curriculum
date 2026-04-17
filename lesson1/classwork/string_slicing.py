9d9b75365c722e8644ea8365ed1523ba71fcc8e0
megabuilder2345 <elliotanselbaer@gmail.com>
2026-04-16 18:38:41 -0700

    fineshed hw 1

--- lesson1/classwork/string_slicing.py
+++ lesson1/classwork/string_slicing.py
@@ -1,7 +1,7 @@
 word="pineapple"
 print("the word is: " + word)
 
-#strings are 0 indexed (first character is index 0)
+# strings are 0 indexed (first character is index 0)
 print("First letter:", word[0])
 print("Second letter: ", word[1])
 
@@ -9,10 +9,10 @@ print("Second letter: ", word[1])
 print("last letter:", word[-1])
 print("second to last letter:", word[-2])
 
-#slicing word[start:stop] ([start, stop])
+# slicing word[start:stop] ([start, stop])
 print(word[0:4])
 print(word[4:9])
 
-#shortcuts:
+# shortcuts:
 print(word[:4]) # from the beginning
 print(word[4:]) # from the end
