065d51a52c33faf4738b2239f0dd754b6aef0774
MegaBuilder2345 <elliotanselbaer@gmail.com>
2026-04-24 17:56:02 -0700

    finesthed himeasdwoirdk 3

--- lesson3/homework/problems.py
+++ lesson3/homework/problems.py
@@ -1,28 +1,40 @@
 # Problem 1
 # Ask the user for a number n.
 # Print all multiples of 3 from 0 to n (including n if it is a multiple of 3).
-
+n=int(input("gimme a number"))
+for i in range(0, n + 1, 3):
+    print(i)
 
 
 # Problem 2
 # Ask the user for a number n.
 # Print the square of every number from 1 to n (1*1, 2*2, ...).
-
+n=int(input("gimme a number"))
+for i in range(1, n + 1):
+    print(i*i)
 
 
 # Problem 3
 # Ask the user for a number n.
 # Print the numbers from n down to 0 (counting down).
+n = int(input("Enter a number: "))
 
+for i in range(n, -1, -1):
+    print(i)
 
 
 # Problem 4
 # Ask the user for a word.
 # Build a new string that repeats the word 5 times with spaces in between.
 # Example: "hi hi hi hi hi"
-
+word = input("Enter a word: ")
+result = (word + " ") * 5
+print(result.strip())
 
 
 # Problem 5
 # Ask the user for a number n.
 # Print how many numbers from 1 to n are even.
+n = int(input("Enter a number: "))
+count = n // 2
+print("There are", count, "even numbers.")
